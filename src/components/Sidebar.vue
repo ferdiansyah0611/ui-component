36edaf2bdc5b3399dbb02b3d75f6a657e9021124
Ferdiansyah <ferdif9996@gmail.com>
2021-05-27 23:48:59 +0700

    support top & bottom sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,8 +4,18 @@
 	</a>
 	<div :id="id" class="menu">
 		<div @click="action" class="opacity bg-black bg-opacity-50 h-screen top-0 left-0 w-full z-30 fixed overflow-auto cursor-pointer hidden"></div>
-		<div :class="sidebar.classes">
+		<div v-if="this.post == 'right' || this.post == 'left'" :class="sidebar.classes">
 			<div class="bg-white w-48 h-screen overflow-auto text-black">
+				<div class="p-3 font-bold text-center">
+					<slot name="head"></slot>
+				</div>
+				<div class="flex flex-wrap">
+					<slot name="link"></slot>
+				</div>
+			</div>
+		</div>
+		<div v-if="this.post == 'bottom' || this.post == 'top'" :class="sidebar.classes">
+			<div class="bg-white w-full h-auto overflow-auto text-black">
 				<div class="p-3 font-bold text-center">
 					<slot name="head"></slot>
 				</div>
@@ -45,6 +55,12 @@ export default{
 		if(this.post == 'left'){
 			this.$data.sidebar.classes = this.$data.sidebar.classes.replace('', ' fixed z-40 top-0 transition-all duration-500 left-0 ml-full')
 		}
+		if(this.post == 'top'){
+			this.$data.sidebar.classes = this.$data.sidebar.classes.replace('', ' fixed z-40 transition-all duration-500 top-0 left-0 mt-full')
+		}
+		if(this.post == 'bottom'){
+			this.$data.sidebar.classes = this.$data.sidebar.classes.replace('', ' fixed z-40 transition-all duration-500 bottom-0 left-0 mb-full')
+		}
 	},
 	methods: {
 		action(e){
@@ -56,16 +72,30 @@ export default{
 				opacity.classList.remove('hidden')
 				if(this.post == 'right'){
 					body.classList.remove('mr-full')
-				}else{
+				}
+				if(this.post == 'left'){
 					body.classList.remove('ml-full')
+				}
+				if(this.post == 'top'){
+					body.classList.remove('mt-full')
+				}
+				if(this.post == 'bottom'){
+					body.classList.remove('mb-full')
 				}
 			}else{
 				this.open = false
 				opacity.classList.add('hidden')
 				if(this.post == 'right'){
 					body.classList.add('mr-full')
-				}else{
+				}
+				if(this.post == 'left'){
 					body.classList.add('ml-full')
+				}
+				if(this.post == 'top'){
+					body.classList.add('mt-full')
+				}
+				if(this.post == 'bottom'){
+					body.classList.add('mb-full')
 				}
 			}
 		}
@@ -78,5 +108,11 @@ export default{
 }
 .ml-full{
 	margin-left: -200vh;
+}
+.mb-full{
+	margin-bottom: -200vh;
+}
+.mt-full{
+	margin-top: -200vh;
 }
 </style>
