66dae123d26df84bda72789fd8c0578c3ea496ea
Ferdiansyah <ferdif9996@gmail.com>
2021-05-28 10:44:37 +0700

    new feature tabs & button icon

---
+++ src/components/ButtonIcon.vue
@@ -0,0 +1,94 @@
+<template>
+	<button :type="role ? role: 'button'" :class="cls"><slot></slot></button>
+</template>
+<script>
+const list = [
+	{type: 'primary', cls: 'bg-blue-500 text-white focus:outline-none focus:ring-4 focus:ring-blue-300'},
+	{type: 'danger', cls: 'bg-red-500 text-white focus:outline-none focus:ring-4 focus:ring-red-300'},
+	{type: 'success', cls: 'bg-green-500 text-white focus:outline-none focus:ring-4 focus:ring-green-300'},
+	{type: 'warning', cls: 'bg-yellow-500 text-white focus:outline-none focus:ring-4 focus:ring-yellow-300'},
+	{type: 'dark', cls: 'bg-black text-white focus:outline-none focus:ring-4 focus:ring-gray-600'},
+	{type: 'light', cls: 'bg-white text-black focus:outline-none focus:ring-4 focus:ring-gray-100 border border-gray-200'},
+	{type: 'secondary', cls: 'bg-gray-200 text-black focus:outline-none focus:ring-4 focus:ring-gray-100 border border-gray-100'},
+]
+export default{
+	name: 'Button UI',
+	props: {
+		type: {
+			type: String, required: true
+		},
+		bg: {
+			type: String, required: false
+		},
+		color: {
+			type: String, required: false
+		},
+		p: {
+			type: String, required: false
+		},
+		ring: {
+			type: String, required: false
+		},
+		sizeRing: {
+			type: String, required: false
+		},
+		size: {
+			type: String, required: false
+		},
+		role: {
+			type: String, required: false
+		},
+	},
+	data(){
+		return{
+			cls: ' focus:outline-none'
+		}
+	},
+	beforeMount(){
+		this.cls = this.cls.replace('', ' flex justify-start')
+		if(this.type !== 'custom'){
+			for (var i = 0; i < list.length; i++) {
+				if(list[i].type == this.type){
+					if(this.size == 'sm'){
+						this.cls = this.cls.replace('', list[i].cls + ' p-2 flex justify-start space-x-2')
+						break;
+					}
+					if(this.size == 'md'){
+						this.cls = this.cls.replace('', list[i].cls + ' p-3 flex justify-start space-x-2')
+						break;
+					}
+					if(this.size == 'lg'){
+						this.cls = this.cls.replace('', list[i].cls + ' p-4 flex justify-start space-x-2')
+						break;
+					}
+					else{
+						this.cls = this.cls.replace('', list[i].cls + ' p-3 flex justify-start space-x-2')
+						break;
+					}
+				}
+			}
+		}else{
+			if(this.bg){
+				this.cls = this.cls.replace('', ' bg-' + this.bg)
+			}
+			if(this.color){
+				this.cls = this.cls.replace('', ' text-' + this.color)
+			}
+			if(this.p){
+				this.cls = this.cls.replace('', ' p-' + this.p)
+			}
+			if(this.ring){
+				this.cls = this.cls.replace('', ' focus:ring-' + this.ring)
+			}
+			if(this.sizeRing){
+				if(this.sizeRing == "1"){
+					this.cls = this.cls.replace('', ' focus:ring')
+				}
+				else{
+					this.cls = this.cls.replace('', ' focus:ring-' + this.sizeRing)
+				}
+			}
+		}
+	}
+}
+</script>
---
+++ src/components/Tabs.vue
@@ -0,0 +1,73 @@
+<template>
+	<div v-if="post == 'x'" :id="id">
+		<div class="grid grid-flow-col">
+			<Button @click="toggle" v-for="(data, key) in title" v-bind:key="key" :type="type" :data-name="data.name">{{data.title}}</Button>
+		</div>
+		<div class="body">
+			<div v-for="(data, key) in title" v-bind:key="key">
+				<div v-if="data.name == this.open" class="w-full block p-3 border">
+					<slot :name="data.name"></slot>
+				</div>
+				<div v-else class="w-full hidden">
+					<slot :name="data.name"></slot>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div v-if="post == 'y'" :id="id" class="flex">
+		<div class="grid grid-flow-row w-1/5">
+			<Button @click="toggle" v-for="(data, key) in title" v-bind:key="key" :type="type" :data-name="data.name">{{data.title}}</Button>
+		</div>
+		<div class="body w-4/5">
+			<div v-for="(data, key) in title" v-bind:key="key">
+				<div v-if="data.name == this.open" class="w-full block p-3 border">
+					<slot :name="data.name"></slot>
+				</div>
+				<div v-else class="w-full hidden">
+					<slot :name="data.name"></slot>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+export default{
+	name: 'Tabs UI',
+	props: {
+		type: {
+			type: String,
+			required: true
+		},
+		title: {
+			type: Array,
+			required: true
+		},
+		default: {
+			type: String,
+			required: true
+		},
+		post: {
+			type: String,
+			required: true
+		},
+	},
+	data(){
+		return{
+			id: 'tabs-' + Math.floor(Math.random() * 100000),
+			defaultOpen: '',
+			open: ''
+		}
+	},
+	beforeMount(){
+		this.open = this.default
+	},
+	methods: {
+		toggle(e){
+			e.preventDefault()
+			if(e.target.dataset.name !== this.open){
+				this.open = e.target.dataset.name
+			}
+		}
+	}
+}
+</script>
