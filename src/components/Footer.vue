24e79b36a568924804b16bf833b1ce2be34c7306
Ferdiansyah <ferdif9996@gmail.com>
2021-06-22 10:40:26 +0700

    new component Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+	<div v-if="pos === 'default'" :class="'w-full flex text-white ' + color">
+		<div class="w-1/2 flex">
+			<slot name="left"></slot>
+		</div>
+		<div class="w-1/2 flex justify-end">
+			<slot name="right"></slot>
+		</div>
+	</div>
+	<div v-if="pos === 'center'" :class="'w-full flex justify-center text-white ' + color">
+		<slot></slot>
+	</div>
+</template>
+<script>
+export default{
+	name: 'Footer UI',
+	props: {
+		pos: {
+			type: String,
+			required: true
+		},
+		color: {
+			type: String,
+			required: true
+		},
+	}
+}
+</script>
---
+++ src/views/component/FooterDoc.vue
@@ -0,0 +1,65 @@
+<template>
+	<div class="w-full bg-white border p-3">
+		<h5 class="mb-3 font-bold text-2xl"># Footer Component</h5>
+		<p class="text-xl tracking-wide">Use UI Components custom button styles for actions in forms, dialogs, and more with support for multiple sizes, states, and more.</p>
+	</div>
+	<div class="w-full bg-white border p-3 mt-2">
+		<h5 class="mb-5 font-bold text-2xl"># Display</h5>
+		<div class="flex w-full flex-wrap mt-2 space-y-2">
+			<Footer pos="default" color="bg-blue-800">
+				<template v-slot:left>
+					<p class="p-4">&copy; {{new Date().getFullYear()}}</p>
+				</template>
+				<template v-slot:right>
+					<a href="/" class="p-4 hover:bg-blue-900">Privacy & Policy</a>
+					<a href="." class="p-4 hover:bg-blue-900">MIT License</a>
+				</template>
+			</Footer>
+			<Footer pos="center" color="bg-red-400">
+				<p class="p-4">&copy; {{new Date().getFullYear()}} Ferdiansyah0611 Corporation</p>
+			</Footer>
+		</div>
+	</div>
+	<div class="w-full bg-white border p-3 mt-2">
+		<h5 class="mb-5 font-bold text-2xl">Example Code</h5>
+		<pre class="flex w-full border p-3 focus:outline-none mb-2" contenteditable="">
+			<code class="w-full overflow-auto text-sm" v-html="example.one"></code>
+		</pre>
+	</div>
+	<div class="w-full bg-white border p-3 mt-2">
+		<h5 class="font-bold text-2xl">Refference</h5>
+		<div class="w-full border-b py-2">
+			<h5 class="font-medium mb-1 text-sm md:text-base"><code>pos (String)</code></h5>
+			<p class="text-sm">A position of footer. List position: default, center.</p>
+		</div>
+		<div class="w-full border-b py-2">
+			<h5 class="font-medium mb-1 text-sm md:text-base"><code>color (String)</code></h5>
+			<p class="text-sm">Class bacground-color.</p>
+		</div>
+	</div>
+</template>
+<script>
+export default{
+	created(){
+		document.title = 'Footer Component | UI Component Docs'
+	},
+	data(){
+		return{
+			example: {
+				one: `<pre class="javascript" style="font-family:monospace;"><span style="color: #339933;">&lt;</span>Footer pos<span style="color: #339933;">=</span><span style="color: #3366CC;">&quot;default&quot;</span> color<span style="color: #339933;">=</span><span style="color: #3366CC;">&quot;bg-blue-800&quot;</span><span style="color: #339933;">&gt;</span>
+	<span style="color: #339933;">&lt;</span>template v<span style="color: #339933;">-</span>slot<span style="color: #339933;">:</span>left<span style="color: #339933;">&gt;</span>
+		<span style="color: #339933;">&lt;</span>p <span style="color: #FF0000;">class</span><span style="color: #339933;">=</span><span style="color: #3366CC;">&quot;p-4&quot;</span><span style="color: #339933;">&gt;&amp;</span>copy<span style="color: #339933;">;</span> <span style="color: #CC0000;">2050</span><span style="color: #339933;">&lt;/</span>p<span style="color: #339933;">&gt;</span>
+	<span style="color: #339933;">&lt;/</span>template<span style="color: #339933;">&gt;</span>
+	<span style="color: #339933;">&lt;</span>template v<span style="color: #339933;">-</span>slot<span style="color: #339933;">:</span>right<span style="color: #339933;">&gt;</span>
+		<span style="color: #339933;">&lt;</span>a href<span style="color: #339933;">=</span><span style="color: #3366CC;">&quot;/&quot;</span> <span style="color: #FF0000;">class</span><span style="color: #339933;">=</span><span style="color: #3366CC;">&quot;p-4 hover:bg-blue-900&quot;</span><span style="color: #339933;">&gt;</span>Privacy <span style="color: #339933;">&amp;</span> Policy<span style="color: #339933;">&lt;/</span>a<span style="color: #339933;">&gt;</span>
+		<span style="color: #339933;">&lt;</span>a href<span style="color: #339933;">=</span><span style="color: #3366CC;">&quot;.&quot;</span> <span style="color: #FF0000;">class</span><span style="color: #339933;">=</span><span style="color: #3366CC;">&quot;p-4 hover:bg-blue-900&quot;</span><span style="color: #339933;">&gt;</span>MIT License<span style="color: #339933;">&lt;/</span>a<span style="color: #339933;">&gt;</span>
+	<span style="color: #339933;">&lt;/</span>template<span style="color: #339933;">&gt;</span>
+<span style="color: #339933;">&lt;/</span>Footer<span style="color: #339933;">&gt;</span>
+<span style="color: #339933;">&lt;</span>Footer pos<span style="color: #339933;">=</span><span style="color: #3366CC;">&quot;center&quot;</span> color<span style="color: #339933;">=</span><span style="color: #3366CC;">&quot;bg-red-400&quot;</span><span style="color: #339933;">&gt;</span>
+	<span style="color: #339933;">&lt;</span>p <span style="color: #FF0000;">class</span><span style="color: #339933;">=</span><span style="color: #3366CC;">&quot;p-4&quot;</span><span style="color: #339933;">&gt;&amp;</span>copy<span style="color: #339933;">;</span> <span style="color: #CC0000;">2050</span> Ferdiansyah0611 Corporation<span style="color: #339933;">&lt;/</span>p<span style="color: #339933;">&gt;</span>
+<span style="color: #339933;">&lt;/</span>Footer<span style="color: #339933;">&gt;</span></pre>`
+			}
+		}
+	}
+}
+</script>
